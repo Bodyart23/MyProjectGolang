a89af082636a43da725b02a43ae100ebf01664af
Bodyart23 <https://github.com/Bodyart23/MyProjectGolang.git>
2019-06-18 19:40:47 +0300

    commit

--- web-app/controller/handlers_test.go
+++ web-app/controller/handlers_test.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"bytes"
 	"github.com/franela/goblin"
 	"net/http"
 	"net/http/httptest"
@@ -29,62 +30,119 @@ func TestGetUsers(t *testing.T) {
 
 			expected := `[{"id":"43","name":"Bogdan","surname":"Degtar","email":"someEmail","gender":"Male","dateregistration":"2019-06-17T00:00:00Z","loan":123.23},{"id":"44","name":"Bob","surname":"Stinger","email":"emailSome","gender":"Male","dateregistration":"2019-06-17T00:00:00Z","loan":321.32}]`
 			if rr.Body.String() == expected {
-				//t.Errorf("handler returned expected body: got %v want %v",
-				//	rr.Body.String(), expected)
-				g.Assert(rr.Body.String()).Equal(expected)
+				t.Errorf("handler returned expected body: got %v want %v",
+					rr.Body.String(), expected)
+				//g.Assert(rr.Body.String()).Equal(expected)
 			}
 		})
 	})
 }
 
-//func TestUsersFilter(t *testing.T) {
-//	g := goblin.Goblin(t)
-//	log.InitLogger()
-//	g.Describe("#UsersFilter", func() {
-//		g.It("should return all users", func() {
-//			req, err := http.NewRequest("GET", "/users/filter/?name=Bogdan", nil)
-//			if err != nil {
-//				t.Fatal(err)
-//			}
-//			rr := httptest.NewRecorder()
-//
-//			handler := http.HandlerFunc(UsersFilter)
-//			handler.ServeHTTP(rr, req)
-//
-//			if status := rr.Code; status != http.StatusOK {
-//				t.Errorf("handler returned wrong status code: got %v want %v",
-//					status, http.StatusOK)
-//			}
-//
-//			expected := `[{"id":"43","name":"Bogdan","surname":"Degtar","email":"someEmail","gender":"Male","dateregistration":"2019-06-17T00:00:00Z","loan":123.23},{"id":"44","name":"Bob","surname":"Stinger","email":"emailSome","gender":"Male","dateregistration":"2019-06-17T00:00:00Z","loan":321.32}]`
-//			if rr.Body.String() == expected {
-//				t.Errorf("handler returned expected body: got %v want %v",
-//					rr.Body.String(), expected)
-//			}
-//		})
-//	})
-//}
-//func TestEventNextHandler(t *testing.T) {
-//	// integration test on http requests to EventNextHandler
-//	log.InitLogger()
-//	request, _ := http.NewRequest("GET", "/users", nil)
-//	var people  = repository.Debtor{}
-//	people.FirstName = "Bill"
-//	people.LastName = request.FormValue("surname")
-//	people.Email = "sdfsdf"
-//	people.Gender = request.FormValue("gender")
-//	people.Loan, _ = strconv.ParseFloat(request.FormValue("loan"),64)
-//	response := httptest.NewRecorder()
-//	people.Insert()
-//	if response.Code != http.StatusOK {
-//	t.Fatalf("Non-expected status code%v:\n\tbody: %v", "200", response.Code)
-//	}
-//}
-//func TestGetUsers(t *testing.T) {
-//	g := goblin.Goblin(t)
-//	log.InitLogger()
-//	g.Describe("#GetUsers", func() {
-//		g.It("should return all users", func() {
-//		})
-//	})
-//}
+func TestCreateUser(t *testing.T) {
+	g := goblin.Goblin(t)
+	log.InitLogger()
+	g.Describe("#CreateUser", func() {
+		g.It("should return 'User was created' ", func() {
+			var jsonStr = []byte(`{"name":"Dobby","surname":"Potter","email":"dob@pot.com","gender":"Male","loan":"123.321"}`)
+
+			req, err := http.NewRequest("POST", "/user/", bytes.NewBuffer(jsonStr))
+			if err != nil {
+				t.Fatal(err)
+			}
+			req.Header.Set("Content-Type", "application/json")
+			rr := httptest.NewRecorder()
+			handler := http.HandlerFunc(CreateUser)
+			handler.ServeHTTP(rr, req)
+			if status := rr.Code; status != http.StatusOK {
+				t.Errorf("handler returned wrong status code: got %v want %v",
+					status, http.StatusOK)
+			}
+			expected := "User was created"
+			if rr.Body.String() == expected {
+				t.Errorf("handler returned unexpected body: got %v want %v",
+					rr.Body.String(), expected)
+			}
+		})
+	})
+}
+func TestUpdateUser(t *testing.T) {
+	g := goblin.Goblin(t)
+	log.InitLogger()
+	g.Describe("#UpdateUser", func() {
+		g.It("should return 'User was updated' ", func() {
+			var jsonStr = []byte(`{"id":44,"name":"Dobby","surname":"Potter","email":"dob@pot.com","gender":"Male","loan":"123.321"}`)
+
+			req, err := http.NewRequest("PUT", "/user/", bytes.NewBuffer(jsonStr))
+			if err != nil {
+				t.Fatal(err)
+			}
+			req.Header.Set("Content-Type", "application/json")
+			rr := httptest.NewRecorder()
+			handler := http.HandlerFunc(UpdateUser)
+			handler.ServeHTTP(rr, req)
+			if status := rr.Code; status != http.StatusOK {
+				t.Errorf("handler returned wrong status code: got %v want %v",
+					status, http.StatusOK)
+			}
+			expected := "User was updated"
+			if rr.Body.String() != expected {
+				t.Errorf("handler returned unexpected body: got %v want %v",
+					rr.Body.String(), expected)
+			}
+		})
+	})
+}
+func TestDeleteUser(t *testing.T) {
+	g := goblin.Goblin(t)
+	log.InitLogger()
+	g.Describe("#DeleteUser", func() {
+		g.It("should return 'User was deleted' ", func() {
+			req, err := http.NewRequest("DELETE", "/user/", nil)
+			if err != nil {
+				t.Fatal(err)
+			}
+			q := req.URL.Query()
+			q.Add("id", "43")
+			req.URL.RawQuery = q.Encode()
+			rr := httptest.NewRecorder()
+			handler := http.HandlerFunc(DeleteUser)
+			handler.ServeHTTP(rr, req)
+			if status := rr.Code; status != http.StatusOK {
+				t.Errorf("handler returned wrong status code: got %v want %v",
+					status, http.StatusOK)
+			}
+			expected := "User was deleted"
+			if rr.Body.String() == expected {
+				t.Errorf("handler returned unexpected body: got %v want %v",
+					rr.Body.String(), expected)
+			}
+		})
+	})
+}
+func TestUsersFilter(t *testing.T) {
+	g := goblin.Goblin(t)
+	log.InitLogger()
+	g.Describe("#UsersFilter", func() {
+		g.It("should return filtering users ", func() {
+			req, err := http.NewRequest("DELETE", "/user/", nil)
+			if err != nil {
+				t.Fatal(err)
+			}
+			q := req.URL.Query()
+			q.Add("name", "Bogdan")
+			req.URL.RawQuery = q.Encode()
+			rr := httptest.NewRecorder()
+			handler := http.HandlerFunc(UsersFilter)
+			handler.ServeHTTP(rr, req)
+			if status := rr.Code; status != http.StatusOK {
+				t.Errorf("handler returned wrong status code: got %v want %v",
+					status, http.StatusOK)
+			}
+			expected := `{"id":"43","name":"Bogdan","surname":"Degtar","email":"someEmail","gender":"Male","dateregistration":"2019-06-17T00:00:00Z","loan":123.23}`
+			if rr.Body.String() == expected {
+				t.Errorf("handler returned unexpected body: got %v want %v",
+					rr.Body.String(), expected)
+			}
+		})
+	})
+}
